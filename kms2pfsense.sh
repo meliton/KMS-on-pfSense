8a20d74bd8de51dd7e12b5a2706e04b4a66d7fab
Mel <m_hinojosa@rocketmail.com>
2023-03-20 20:16:21 -0500

    Update kms2pfsense.sh

--- kms2pfsense.sh
+++ kms2pfsense.sh
@@ -27,7 +27,7 @@ case "$(uname -m 2>/dev/null | grep -c "amd64" )" in
    exit 1 ;;
 esac
 
-# check for OS version 11.xx or 12.xx
+# check for OS version 11.xx through 14.xx
 case "$(uname -r 2>/dev/null | grep -c "11.\|12.\|13.\|14." )" in
    1) echo " PASSED - Operating system 11.xx or higher" ;;
    *) echo " FAILED - Wrong OS version. Not at least 11.xx" ; 
